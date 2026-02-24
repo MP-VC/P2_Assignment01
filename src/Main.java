f7ab078441ca0e27fb50948cea06a5b62c660937
2519989 <2519989@edu.vaniercollege.qc.ca>
2026-02-24 14:59:07 -0500

    Did minrow and mincol

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
 import task1.*;
 import task2.*;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        double[][] numbs = new double[][]{{1.2,1.3},{1.34}};
+        double[][] numbs = new double[][]{
+                {1.2,  1.3, 67,   9,   -2},
+                {1.34, 1.2, 1.1,  1.58, 43,5,5,-99,5,2,4,6},
+                {3,    4,   21,   6,    0}
+        };
         System.out.println(Util2DArray.min(numbs));
+        System.out.println(Arrays.toString(Util2DArray.minOfRow(numbs)));
+        System.out.println(Arrays.toString(Util2DArray.minOfCol(numbs)));
     }
 }
--- src/task1/Util2DArray.java
+++ src/task1/Util2DArray.java
@@ -1,5 +1,7 @@
 package task1;
 
+import java.util.Arrays;
+
 public class Util2DArray {
     public static double min(double[][] numbs) {
         double min = Double.POSITIVE_INFINITY;
@@ -9,5 +11,38 @@ public class Util2DArray {
             }
         }
         return min;
+    }
+    public static double[] minOfRow(double[][] numbs) {
+        double[] dob = new double[numbs.length];
+        double min = Double.POSITIVE_INFINITY;
+        int i = 0;
+        for(double[] d : numbs) {
+            for(double e : d) {
+                if (e <= min) min = e;
+            }
+            dob[i] = min;
+            i++;
+            min = Double.POSITIVE_INFINITY;
+        }
+        return dob;
+    }
+    public static double[] minOfCol(double[][] numbs) {
+        int length = Integer.MIN_VALUE;
+        for(double[] d : numbs) {
+            if(d.length > length) length = d.length;
+        }
+
+        double[][] transpose = new double[length][length];
+
+        for (double[] doubles : transpose) {
+            Arrays.fill(doubles, Double.POSITIVE_INFINITY);
+        }
+        for(int x = 0; x < length; x++) {
+            for(int y = 0; y < length; y++) {
+                try { transpose[x][y] = numbs[y][x]; }
+                catch(IndexOutOfBoundsException _) {}
+            }
+        }
+        return minOfRow(transpose);
     }
 }
