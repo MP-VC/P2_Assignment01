6774406a8b6d6868ca2b3e5f5047567ee08fa3e2
2519989 <2519989@edu.vaniercollege.qc.ca>
2026-02-24 12:57:20 -0500

    did task 1 question 1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import task1.*;
 import task2.*;
 public class Main {
     public static void main(String[] args) {
-
+        double[][] numbs = new double[][]{{1.2,1.3},{1.34}};
+        System.out.println(Util2DArray.min(numbs));
     }
 }
--- src/task1/Util2DArray.java
+++ src/task1/Util2DArray.java
@@ -1,4 +1,13 @@
 package task1;
 
 public class Util2DArray {
+    public static double min(double[][] numbs) {
+        double min = Double.POSITIVE_INFINITY;
+        for(double[] d : numbs) {
+            for(double e : d) {
+                if (e < min) min = e;
+            }
+        }
+        return min;
+    }
 }
